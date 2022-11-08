51bd1f5804ba911e5698dddc1d04b048b768b0ab
DarkNight-sibp <sara.b.pereira8@gmail.com>
2022-11-08 08:51:52 +0000

    Tutorial - Calculating Trajectories Part 2

--- Assets/Velocity - Tanks/Scripts/FireShell.cs
+++ Assets/Velocity - Tanks/Scripts/FireShell.cs
@@ -3,39 +3,41 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class FireShell : MonoBehaviour {
+public class FireShell : MonoBehaviour
+{
 
     public GameObject bullet;
     public GameObject turret;
     public GameObject enemy;
     public Transform turretBase;
 
-    private float speed = 15.0f;
-    private float rotSpeed = 5.0f;
-    private float moveSpeed = 1.0f;
+    float speed = 15;
+    float rotSpeed = 2;
 
-    static float delayReset = 0.2f;
-    float delay = delayReset;
-    private object normalized;
-
-    void CreateBullet() {
+    void CreateBullet()
+    {
 
         GameObject shell = Instantiate(bullet, turret.transform.position, turret.transform.rotation);
         shell.GetComponent<Rigidbody>().velocity = speed * turretBase.forward;
     }
 
-    float? RotateTurret() {
+    float? RotateTurret()
+    {
 
-        float? angle = CalculateAngle(false);
-
-        if (angle != null) {
-
+        float? angle = CalculateAngle(false); //did i acidentally downlaod the entire thing cause i couldnt find the fireshell script???
+        {
+            Vector3 targetDir = enemy.transform.position = this.transform.position;
+            float y = targetDir.y = 0f; // or did i just upload part of it??
+        }
+        if (angle != null)
+        {
             turretBase.localEulerAngles = new Vector3(360.0f - (float)angle, 0.0f, 0.0f);
         }
         return angle;
     }
 
-    float? CalculateAngle(bool low) {
+    float? CalculateAngle(bool low)
+    {
 
         Vector3 targetDir = enemy.transform.position - this.transform.position;
         float y = targetDir.y;
@@ -43,9 +45,10 @@ public class FireShell : MonoBehaviour {
         float x = targetDir.magnitude - 1.0f;
         float gravity = 9.8f;
         float sSqr = speed * speed;
-        float underTheSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
+        float underTheSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr); // ignore notes above, you did acidentally download the code cause you could find the fireshell. well makec the code run i guess, its not running on unity
 
-        if (underTheSqrRoot >= 0.0f) {
+        if (underTheSqrRoot >= 0.0f)
+        {
 
             float root = Mathf.Sqrt(underTheSqrRoot);
             float highAngle = sSqr + root;
@@ -53,18 +56,24 @@ public class FireShell : MonoBehaviour {
 
             if (low) return (Mathf.Atan2(lowAngle, gravity * x) * Mathf.Rad2Deg);
             else return (Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
-        } else
+        }
+        else
             return null;
     }
-    object Update() 
+    object Update()
     {
 
         Vector3 direction = (enemy.transform.position - this.transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0.0f, direction.z));
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * rotSpeed);
-        { if (Input.GetKeyDown(KeyCode.space))
-                CreateBullet();
+        RotateTurret();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateBullet();
+            return 0; 
         }
+    }
+}
 
         
         // delay -= Time.deltaTime;
