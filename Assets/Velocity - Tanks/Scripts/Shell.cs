b5bc46c89ae1b1a5bd8fd80f2942251a933e9c8c
DarkNight-sibp <sara.b.pereira8@gmail.com>
2022-11-08 08:29:41 +0000

    Tutorial - Calculating Trajectories Part 1

--- Assets/Velocity - Tanks/Scripts/Shell.cs
+++ Assets/Velocity - Tanks/Scripts/Shell.cs
@@ -2,41 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shell : MonoBehaviour
-{
+public class Shell : MonoBehaviour {
+
     public GameObject explosion;
-    float speed = 0f;
-    float yspeed = 0f;
-    float mass = 10;
-    float force = 3;
+    float speed = 0.0f;
+    float ySpeed = 0.0f;
+    float mass = 30.0f;
+    float force = 4.0f;
+    float drag = 1.0f;
     float gravity = -9.8f;
-    float graAc;
-    float drag = 1;
+    float gAccel;
     float acceleration;
 
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.tag == "tank")
-        {
+
+    void OnCollisionEnter(Collision col) {
+
+        if (col.gameObject.tag == "tank") {
             GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
             Destroy(exp, 0.5f);
             Destroy(this.gameObject);
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    private void Start() {
+
         acceleration = force / mass;
-        speed += acceleration * 1;
-        graAc = gravity / mass;
+        speed += acceleration * 1.0f;
+        gAccel = gravity / mass;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-         speed *= (1 - Time.deltaTime * drag);
-         yspeed += graAc * Time.deltaTime;
-        this.transform.Translate(0, yspeed, speed * Time.deltaTime);
+    void LateUpdate() {
+
+        speed *= (1 - Time.deltaTime * drag);
+        ySpeed += gAccel * Time.deltaTime;
+        this.transform.Translate(0.0f, ySpeed, speed);
     }
 }
