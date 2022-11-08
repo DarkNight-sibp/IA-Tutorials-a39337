dde04e985157ff137a6a5d4618422d24c4d0f17b
DarkNight-sibp <sara.b.pereira8@gmail.com>
2022-11-08 05:47:29 +0000

    Tutorial - Time

--- Assets/Time Starter/Script/SecondsUpdate.cs
+++ Assets/Time Starter/Script/SecondsUpdate.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class SecondsUpdate : MonoBehaviour
 {
-    float timeStartOffset = 0; // Variable to store seconds elapsed since starting.
-    bool gotStartTime = false; // Variable to ensure offset is only changed once.
+    float timeStartOffset = 0; 
+    bool gotStartTime = false; 
 
     void Update()
     {
-        // Using real-word time to move the character.
         if (!gotStartTime)
-        {
-            timeStartOffset = Time.realtimeSinceStartup;
-            gotStartTime = true;
-        }
-
-        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 
-                                    Time.realtimeSinceStartup - timeStartOffset);
+        { 
+             timeStartOffset = Time.realtimeSinceStartup;
+             gotStartTime = true;
+                }
+       this.transform.position = new Vector3(this.transform.position.x,
+                                             this.transform.position.y,
+                                             Time.realtimeSinceStartup - timeStartOffset);
     }
 }
