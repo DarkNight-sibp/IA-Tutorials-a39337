b12228b248a8d49daa7b00ecfe2b96736e17f500
DarkNight-sibp <sara.b.pereira8@gmail.com>
2022-11-08 06:04:17 +0000

    Tutorial -Normalising Movement with Time

--- Assets/Time Starter/Script/UpdateMove.cs
+++ Assets/Time Starter/Script/UpdateMove.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class UpdateMove : MonoBehaviour
 {
-    float speed = 2.0f;
+    public float speed = 0.5f; //Vai mais depressa, not slower?? note 2 - this one is goig faster even though they all have the same number in the speed code???.the code looks the same as in the titorial so idk
     void Update()
     {
-        this.transform.Translate(0, 0, 0.01f);
+        this.transform.Translate(0, 0, Time.deltaTime * speed);
     }
 }
