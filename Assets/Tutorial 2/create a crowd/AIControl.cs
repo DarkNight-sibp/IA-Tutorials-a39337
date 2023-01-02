2041dfa27d07172355de04d38879d7e270094482
DarkNight-sibp <sara.b.pereira8@gmail.com>
2023-01-02 23:21:52 +0000

    AI Tutorial - crowd managment - creating a city crowd - part 1

---
+++ Assets/Tutorial 2/create a crowd/AIControl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    GameObject[] goalLocations; 
+    NavMeshAgent agent; 
+    Animator anim; 
+    
+    // Use this for initialization
+    void Start () { 
+        agent = this.GetComponent<NavMeshAgent>(); 
+        goalLocations= GameObject.FindGameObjectsWithTag("goal"); 
+        int i= Random.Range(0, goalLocations.Length); 
+        agent.SetDestination (goalLocations [i].transform.position); 
+        anim = this.GetComponent<Animator>(); 
+        anim.SetTrigger("isWalking");
+
+        void Update () {
+            if (agent.remainingDistance < 1) {
+                int i = Random.Range(0, goalLocations.Length);
+                agent.SetDestination(goalLocations[i].transform.position);
+            }
+        }
+    }
+}
